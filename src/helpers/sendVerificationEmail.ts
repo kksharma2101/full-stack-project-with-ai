beeca2a591b45bc3bac02ab50dcd3c059d92d149
kamal sharma <kamalbsr2101@gmail.com>
2024-06-25 20:10:28 +0530

    create api in sign-up route and sending email functionality

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "@/../emails/VerificationEmail";
+import { ApiResponse } from "@/types/apiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        const sendEmail = await resend.emails.send({
+            from: '<onboarding@resend.dev>',
+            to: email,
+            subject: 'Verification code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+
+        return { success: true, message: "Verification email send successfully" }
+
+    } catch (emailError) {
+        console.error("Error in verification email", emailError)
+        return { success: false, message: "Faild to verify email" }
+    }
+}
